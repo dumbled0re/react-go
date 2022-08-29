d4e269504a2dcc6f7d71c0969b14e798282d6f8b
dumbled0re <s.r19990706@gmail.com>
2022-08-29 22:57:00 +0900

    update: events追加

--- test-app/src/AppContent.js
+++ test-app/src/AppContent.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react";
 
 export default class AppContent extends Component {
+  anotherFunction = () => {
+    console.log("another function");
+  };
+
+  leftParagraph = () => {
+    console.log("left the paragraph");
+  };
+
+  fetchList = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        let posts = document.getElementById("post-list");
+
+        json.forEach(function (obj) {
+          let li = document.createElement("li");
+          li.appendChild(document.createTextNode(obj.title));
+          posts.appendChild(li);
+        });
+      });
+  };
+
   render() {
     return (
       <p>
         This is the content.
+        <hr />
         <br />
+        <p
+          // マウスが当たったら
+          onMouseEnter={this.anotherFunction}
+          // マウスが離れたら
+          onMouseLeave={this.leftParagraph}
+        >
+          This is some text
+        </p>
+        {/* ボタンがクリックされたらfetchListを実行 */}
+        <button onClick={this.fetchList} className="btn btn-primary">
+          Fetch Data
+        </button>
+        <hr />
+        <ul id="post-list"></ul>
       </p>
     );
   }
