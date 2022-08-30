778aae5566cec58063eafb7f594fb8aff7417409
dumbled0re <s.r19990706@gmail.com>
2022-08-30 17:19:56 +0900

    update: ルーティング追加

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -1,8 +1,16 @@
-import React, { Component, Fragment } from "react";
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 import Movies from "./components/Movies";
 import Home from "./components/Home";
 import Admin from "./components/Admin";
+import Categories from "./components/Categories";
+import OneMovie from "./components/OneMovie";
 
 export default function App() {
   return (
@@ -24,6 +32,9 @@ export default function App() {
                   <Link to="/movies">Movies</Link>
                 </li>
                 <li className="list-group-item">
+                  <Link to="/by-category">Categories</Link>
+                </li>
+                <li className="list-group-item">
                   <Link to="/admin">Manage Catalogue</Link>
                 </li>
               </ul>
@@ -32,9 +43,23 @@ export default function App() {
 
           <div className="col-md-10">
             <Switch>
+              <Route path="/movies/:id" component={OneMovie} />
               <Route path="/movies">
                 <Movies />
               </Route>
+              <Route exact path="/by-category">
+                <CategoryPage />
+              </Route>
+              <Route
+                exact
+                path="/by-category/drama"
+                render={(props) => <Categories {...props} title={`Drama`} />}
+              />
+              <Route
+                exact
+                path="/by-category/comedy"
+                render={(props) => <Categories {...props} title={`Comedy`} />}
+              />
               <Route path="/admin">
                 <Admin />
               </Route>
@@ -46,5 +71,24 @@ export default function App() {
         </div>
       </div>
     </Router>
+  );
+}
+
+function CategoryPage() {
+  //
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Categories</h2>
+      <ul>
+        <li>
+          <Link to={`${path}/comedy`}>Comedy</Link>
+        </li>
+        <li>
+          <Link to={`${url}/drama`}>Drama</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
