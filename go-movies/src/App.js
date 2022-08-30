e7475621b43f3478bfa5cecd5e89b1bde1844ee6
dumbled0re <s.r19990706@gmail.com>
2022-08-30 14:59:59 +0900

    feat: ルーティング追加

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -1,31 +1,50 @@
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <h1 className="mt-3">Go Watch a Movie!</h1>
-        <hr className="mb-3"></hr>
-      </div>
+import React, { Component, Fragment } from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Movies from "./components/Movies";
+import Home from "./components/Home";
+import Admin from "./components/Admin";
 
-      <div className="row">
-        <div className="col-md-2">
-          <nav>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <a href="/">Home</a>
-              </li>
-              <li className="list-group-item">
-                <a href="/movies">Movies</a>
-              </li>
-              <li className="list-group-item">
-                <a href="/admin">Manage Catalogue</a>
-              </li>
-            </ul>
-          </nav>
+export default function App() {
+  return (
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+          <hr className="mb-3"></hr>
         </div>
-        <div className="col-md-10"></div>
+
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin">Manage Catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="col-md-10">
+            <Switch>
+              <Route path="/movies">
+                <Movies />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
-
-export default App;
