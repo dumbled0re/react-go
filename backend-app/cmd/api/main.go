3c1302facf4cf41f2e0bad1c15b862979f380b01
dumbled0re <s.r19990706@gmail.com>
2022-09-02 17:17:57 +0900

    feat: DB接続追加

--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type AppStatus struct {
@@ -34,11 +41,19 @@ func main() {
 	// 引数は変数のポインタ(メモリのアドレス値)、フラグの名前、デフォルト値、使い方の説明
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
+	// flag.StringVar(&cfg.db.dsn, "dsn", "postgres://[role name]:[password]@localhost/[db name]?sslmode=disable", "Postgres connection string")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://ritsushi:@localhost/go_movies?sslmode=disable", "Postgres connection string")
 	// Parse()でそれぞれの変数にアクセス可能
 	flag.Parse()
 
 	// Loggerオブジェクトを生成して出力フォーマットを設定する
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -58,9 +73,27 @@ func main() {
 
 	// :の前に何も書かなければローカルになる、第一引数にserverを立ち上げるポートを記載
 	// 第二引数はハンドラーを渡す(nilの場合はnot-foundを返すハンドラーなのでその前にHandleFuncでハンドラーを渡す必要がある)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	// err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// コンテキスト&5秒のタイムアウト生成
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
