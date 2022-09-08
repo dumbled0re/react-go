f5afd0b5adc19883d49ba8084981acda4638addc
dumbled0re <s.r19990706@gmail.com>
2022-09-08 17:03:45 +0900

    feat: テキストのコンポーネント追加

--- go-movies/src/components/EditMovie.jsx
+++ go-movies/src/components/EditMovie.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import "./EditMovie.css";
 import Input from "./form-components/Input";
+import TextArea from "./form-components/TextArea";
 
 export default class EditMovie extends Component {
   state = {
@@ -124,19 +125,13 @@ export default class EditMovie extends Component {
             handleChange={this.handleChange}
           />
 
-          <div className="mb-3">
-            <label htmlFor="description" className="form-label">
-              Description
-            </label>
-            <textarea
-              className="form-control"
-              id="description"
-              name="description"
-              rows="3"
-              onChange={this.handleChange}
-              value={movie.description}
-            />
-          </div>
+          <TextArea
+            title={"Description"}
+            name={"description"}
+            value={movie.description}
+            rows={"3"}
+            handleChange={this.handleChange}
+          />
 
           <hr />
 
