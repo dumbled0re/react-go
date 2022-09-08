c19902ba23b810a80639a9868aa1286c86c96f7f
dumbled0re <s.r19990706@gmail.com>
2022-09-08 16:42:10 +0900

    feat: フォームの追加

--- go-movies/src/App.jsx
+++ go-movies/src/App.jsx
@@ -12,6 +12,7 @@ import Admin from "./components/Admin";
 import OneMovie from "./components/OneMovie";
 import Genres from "./components/Genres";
 import OneGenre from "./components/OneGenre";
+import EditMovie from "./components/EditMovie";
 
 export default function App() {
   return (
@@ -36,6 +37,9 @@ export default function App() {
                   <Link to="/genres">Genres</Link>
                 </li>
                 <li className="list-group-item">
+                  <Link to="/admin/add">Add movie</Link>
+                </li>
+                <li className="list-group-item">
                   <Link to="/admin">Manage Catalogue</Link>
                 </li>
               </ul>
@@ -55,6 +59,8 @@ export default function App() {
               <Route exact path="/genres">
                 <Genres />
               </Route>
+
+              <Route path="/admin/add" component={EditMovie} />
 
               <Route path="/admin">
                 <Admin />
