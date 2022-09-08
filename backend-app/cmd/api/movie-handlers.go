ca6cd12c93cd534735aabcc5a26fd1770e857ec1
dumbled0re <s.r19990706@gmail.com>
2022-09-08 21:09:02 +0900

    update: フロントからバックエンドにデータを送る

--- backend-app/cmd/api/movie-handlers.go
+++ backend-app/cmd/api/movie-handlers.go
@@ -86,12 +86,20 @@ func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+func (app *application) editmovie(w http.ResponseWriter, r *http.Request) {
+	type jsonResp struct {
+		OK bool `json:"ok"`
+	}
 
-}
+	ok := jsonResp{
+		OK: true,
+	}
 
-func (app *application) updatetMovie(w http.ResponseWriter, r *http.Request) {
-
+	err := app.writeJSON(w, http.StatusOK, ok, "response")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
